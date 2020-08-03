535ec5ec94a0d31d419858edec5f83d8b26c1ca6
jonathan orlen pellonodu <wahyu.orlen@hotmail.com>
2020-08-03 13:58:32 +0700

    add cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors')
 // import mongoose
 const mongoose = require('mongoose');
 mongoose.connect("mongodb+srv://dbUser:dbUserPassword@cluster0.89djm.mongodb.net/db_staycation?retryWrites=true&w=majority", {
@@ -34,6 +35,7 @@ app.use(session({
   cookie: { maxAge: 60000 }
 }));
 app.use(flash());
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
